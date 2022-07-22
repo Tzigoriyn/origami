ca06e1f12ab1d7deb4bac06b74c143be758d8d05
Tzigoriyn <tzigoriyn@gmail.com>
2022-07-22 23:46:09 +0300

    Main Menu create

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,17 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable react/jsx-no-comment-textnodes */
 import Header from "./components/Header";
-// import Menu from "./components/Menu"
+import Menu from "./components/Menu"
 import style from './App.module.css';
 
 function App() {
   return (
     <div className={style.app}>
-      <Header className={style.headerListItem}/>
+      <Header className={style.headerListItem} />
 
-      {/* <Menu /> */}
+      <div className={style.cotainer}>
+        <Menu className={style.container} />
+      </div>
 
     </div>
   );
--- client/src/components/Menu/Menu.js
+++ client/src/components/Menu/Menu.js
@@ -1,7 +1,24 @@
-// import style from './Menu.module.css';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import style from './Menu.module.css';
+
 const Menu = () => {
+    console.log("Ko stana s Menu");
     return (
-        <h1>Start Menu</h1>
+        <aside className={style.aside}>
+            <ul>
+                <li className={style.listItem}><a href="#">Going to 1</a></li>
+                <li className={style.listItem}><a href="#">Going to 2</a></li>
+                <li className={style.listItem}><a href="#">Going to 3</a></li>
+                <li className={style.listItem}><a href="#">Going to 4</a></li>
+                <li className={style.listItem}><a href="#">Going to 5</a></li>
+                <li className={style.listItem}><a href="#">Going to 6</a></li>
+                <li className={style.listItem}><a href="#">Going to 7</a></li>
+                <li className={style.listItem}><a href="#">Going to 8</a></li>
+                <li className={style.listItem}><a href="#">Going to 9</a></li>
+                <li className={style.listItem}><a href="#">Going to 10</a></li>
+                <li className={style.listItem}><a href="#">Going to 11</a></li>
+            </ul>
+        </aside>
     )
 }
 
