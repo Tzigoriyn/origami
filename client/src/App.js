b5007f4a5f6f4c7d3c6de7dc8b4a6cde65065ae7
Tzigoriyn <tzigoriyn@gmail.com>
2022-07-23 11:02:45 +0300

    Refactoring App Func. to Class

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,45 @@
+/* eslint-disable no-useless-constructor */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable react/jsx-no-comment-textnodes */
+import { Component } from "react"
 import Header from "./components/Header";
 import Menu from "./components/Menu";
 import Main from "./components/Main";
 import style from './App.module.css';
 
-function App() {
-  return (
-    <div className={style.app}>
-      <Header />
+// function App() {
+//   return (
+//     <div className={style.app}>
+//       <Header />
 
-      <div className={style.container}>
-        <Menu />
+//       <div className={style.container}>
+//         <Menu />
 
-        <Main />
+//         <Main />
+//       </div>
+
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className={style.app}>
+        <Header />
+
+        <div className={style.container}>
+          <Menu />
+
+          <Main />
+        </div>
+
       </div>
-
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
